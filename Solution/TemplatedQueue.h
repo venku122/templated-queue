96c7533c9ba6e9826010bef5262307bdcfa9f61e
T.J. Tarazevits <tarazevits@gmail.com>
2017-09-12 12:13:32 -0400

    Created Header file for Templated Queue

---
+++ Solution/TemplatedQueue.h
@@ -0,0 +1,49 @@
+#ifndef _TEMPLATED_QUEUE_H
+#define _TEMPLATED_QUEUE_H
+
+// Rule of Three
+template<class T>
+
+class Templated_Queue {
+public:
+	// Constructor
+	Templated_Queue() {
+
+	}
+	// Copy Constructor
+	Templated_Queue(const Templated_Queue& other) {
+
+	}
+	// Copy Assignment Operator
+	Templated_Queue& operator=(const Templated_Queue& other) {
+
+	}
+	// Destructor
+	~Templated_Queue() {
+
+	}
+
+	// Push
+	void push(T newData) {
+
+	}
+	// Pop
+	T pop() {
+
+	}
+	// Print
+	print() {
+
+	}
+	// GetSize
+	GetSize() {
+
+	}
+	// IsEmpty
+	IsEmpty() {
+
+	}
+};
+#endif
+
+
